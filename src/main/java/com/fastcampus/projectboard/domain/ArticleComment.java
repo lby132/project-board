973303229e7ba1b19966b01e36fe4907e49f8d4d
lby132 <quddbscjswow@naver.com>
2024-05-08 23:44:45 +0900

    #6 - 도메인 클래스 설계 (게시글, 댓글)
    기초적인 게시글과 댓글 표현이 가능하도록 함
    DB 와 연동을 위한 추가적인 표현은 추후에 업데이트될 수 있음

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public class ArticleComment {
     private Long id;
     private Article article; // 게시글 (ID)
-    private String content;
+    private String content; // 본문
 
     private LocalDateTime createdAt;
     private String createdBy; // 생성자
